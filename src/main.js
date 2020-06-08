d8f46d161f37eda271836c9bf1e65868f6465e58
青空 <1057605824@qq.com>
2020-06-08 17:26:45 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+
+
+// 引入ElementUI
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 引入路由
+import router from "./router/router";
+import VueRouter from "vue-router";
+Vue.use(VueRouter)
+
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
